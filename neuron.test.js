ecc054aaccc6c35e76c7079aa9cb468ba6fea120
Dennis Hodges <fermentationist@gmail.com>
2018-07-31 15:07:37 -0500

    Added Jest for testing. Implementing tests to Neuron subclasses and logistical functions.

---
+++ neuron.test.js
@@ -0,0 +1,100 @@
+const {adjustedRandomGaussian,
+		reLu,
+		softmax,
+		crossEntropyCostFunction,
+		Neuron,
+		InputNeuron,
+		HiddenNeuron,
+		OutputNeuron,
+		Layer} = require("./tictactoe-ai.js");
+// const HiddenNeuron = require("./tictactoe-ai.js").HiddenNeuron;
+// const OutputNeuron = require("./tictactoe-ai.js").OutputNeuron;
+const randomGaussian = require("random").normal(mu = 0, sigma = 1);
+
+const inputNeuron = new InputNeuron([1]);
+test("01 new InputNeuron([1]).inputs = [1]", () => {
+	expect(inputNeuron.inputs).toEqual([1]);
+});
+test("02 new InputNeuron([1]).outputSignal = [1]", () => {
+	expect(inputNeuron.outputSignal).toEqual([1]);
+});
+test("03 new InputNeuron([1]).weightedInputs = [1]", () => {
+	expect(inputNeuron.weightedInputs).toEqual([1]);
+});
+test("04 new InputNeuron([1]).activatedInputs = [1]", () => {
+	expect(inputNeuron.activatedInputs).toEqual([1]);
+});
+test("05 new InputNeuron([1]).activationFn is null", () => {
+	expect(inputNeuron.activationFn).toBeNull();
+});
+
+// ====================================================
+const rand = Math.floor(Math.random() * 9) + 1;
+const testInputs = Array(rand).fill(0);
+const numInputs = testInputs.length;
+const zeroedNeuron = new HiddenNeuron(testInputs, softmax);
+
+test("06 zeroed HiddenNeuron(testInputs, softmax).inputs = testInputs", () => {
+	expect(zeroedNeuron.inputs).toEqual(testInputs);
+});
+test("07 zeroed HiddenNeuron(testInputs, softmax).outputSignal = 1", () => {
+	expect(zeroedNeuron.outputSignal).toEqual([1]);
+});
+test("08 zeroed HiddenNeuron(testInputs, softmax).weightedInputs = testInputs", () => {
+	expect(zeroedNeuron.weightedInputs.reduce((a,b) => a + b)).toEqual(0);
+	expect(zeroedNeuron.weightedInputs).toHaveLength(numInputs);
+});
+test("09 zeroed HiddenNeuron(testInputs, softmax).activatedInputs = Array(numInputs).fill(1/numInputs)", () => {
+	const output = Array(numInputs).fill(1/numInputs);
+	expect(zeroedNeuron.activatedInputs).toEqual(output);
+});
+test("10 HiddenNeuron(testInputs).activationFn is truthy", () => {
+	expect(zeroedNeuron.activationFn).toBeTruthy();
+});
+
+// ====================================================
+const reLuNeuron = new HiddenNeuron([0,1,2, -1], reLu);
+const Weights = reLuNeuron.weights;
+
+test("11 HiddenNeuron([0,1,2,-1], reLu).activationFn.name to be 'reLu'", () => {
+	expect(reLuNeuron.activationFn.name).toEqual("reLu");
+});
+test("12 HiddenNeuron([0,1,2,-1], reLu).inputs = [0,1,2]", () => {
+	expect(reLuNeuron.inputs).toEqual([0,1,2, -1]);
+});
+test("13 HiddenNeuron([0,1,2,-1], reLu).weightedInputs = [0,reLuNeuron.weights[1],2 * reLuNeuron.weights[2], reLuNeuron.weights[3] * -1]", () => {
+	expect(reLuNeuron.weightedInputs).toEqual([0, reLuNeuron.weights[1], (reLuNeuron.weights[2] * 2), reLuNeuron.weights[3] * -1]);
+});
+test("14 HiddenNeuron([0,1,2,-1], reLu).activatedInputs = [0,reLuNeuron.weights[1],2 * reLuNeuron.weights[2]]", () => {
+	let a2 = reLu(1 * reLuNeuron.weights[1]);
+	let a3 = reLu(2 * reLuNeuron.weights[2]);
+	let a4 = reLu(-1 * reLuNeuron.weights[3]);
+	expect(reLuNeuron.activatedInputs).toEqual([0, a2, a3, a4]);
+});
+test("15 HiddenNeuron([0,1,2,-1], reLu).outputSignal", () => {
+	let [w1, w2, w3, w4] = reLuNeuron.weights;
+	let b = reLuNeuron.bias;
+	let desiredOutput = reLu((0 * w1 + b) + (1 * w2 + b) + (2 * w3 + b) + (-1 * w4 + b));
+	expect(reLuNeuron.outputSignal).toBeCloseTo(desiredOutput, 4);
+});
+
+
+
+
+
+console.log('\n\n\ninputNeuron.activationFn', inputNeuron.activationFn);
+
+test("16 softmax(number) returns [1]", () => {
+	expect(softmax(rand)).toEqual([1]);
+});
+
+test("17 adjustedRandomGaussian(4, 'reLu') returns a non-zero number", () => {
+	let testResult = adjustedRandomGaussian(4, "reLu");
+	expect(typeof testResult).toBe("number");
+	expect(testResult).not.toEqual(0);
+});
+
+
+
+
+
--- tictactoe-ai.js
+++ tictactoe-ai.js
@@ -1,17 +1,21 @@
 const game = require("./tictactoe.js");
-const math = require("mathjs");
 const trainingData = require("./tictactoe-simulations.json");
-const randomGaussian = require("random").normal(mu = 0, sigma = 1);
-
-const mean = 0;
-const variance = 1;
-const sample = randomGaussian();
-console.log('sample', sample);
-let weight = Number.parseFloat(sample).toPrecision(4);
-console.log('weight', weight);
-
 
 const NeuralNetwork = (() => {
+	const randomGaussian = require("random").normal(mu = 0, sigma = 1);
+	const math = require("mathjs");
+
+	const adjustedRandomGaussian = (inputLayerSize, activationFnName) => {
+		const rnd = randomGaussian();
+		// console.log('\nrnd', rnd)
+		if (activationFnName.toLowerCase() === "relu") {
+			return (rnd * Math.sqrt(2/inputLayerSize));
+		}
+		if (activationFnName.toLowerCase() === "softmax" || "tanh") {
+			return (rnd * Math.sqrt(1/inputLayerSize));
+		}
+		return rnd;	
+	}
 
 	const reLu = (input, derivative = false, a = .01) => {
 		if (derivative) {
@@ -47,7 +51,6 @@ const NeuralNetwork = (() => {
 
 	const crossEntropyCostFunction = (prediction, label, derivative = false) => {
 		let predictions = Array.isArray(prediction) ? prediction : [prediction];
-		console.log('prediction, label, derivative', prediction, label, derivative)
 		if (derivative) {
 			return crossEntropyPrime(predictions, label);
 		}
@@ -58,16 +61,6 @@ const NeuralNetwork = (() => {
 		return predictions.map(prediction => -1 * label / prediction);
 	}
 
-	const adjustedRandomGaussian = (inputLayerSize, activationFn, precision = 4) => {
-		const rnd = randomGaussian();
-		if (activationFn.toLowerCase() === "relu") {
-			return (rnd * Math.sqrt(2/inputLayerSize)).toFixed(precision);
-		}
-		if (activationFn.toLowerCase() === "softmax" || "tanh") {
-			return (rnd * Math.sqrt(1/inputLayerSize)).toFixed(precision);
-		}
-		return rnd.toFixed(precision);	
-	}
 	// ========================================================================
 
 
@@ -78,58 +71,40 @@ const NeuralNetwork = (() => {
 			this.inputs = inputLayer || [];
 			this.learningRate = 0.5;
 			this.costFn = costFn;
-			if (this.inputs.length > 1) {
-				this.weights = inputLayer.map((input) => adjustedRandomGaussian(inputLayer.length, this.activationFn.name));
-				this.bias = 0;
-			}
+			this.weights = this.activationFn ? inputLayer.map((input) => adjustedRandomGaussian(inputLayer.length, this.activationFn.name)) : null;
+			this.bias = 0.001;
 		}
 		get weightedInputs () {
-			// console.log("weightedInputs called with this.inputs= " + this.inputs + " and this.weights= " + this.weights + " and this.bias= " + this.bias);
-			return math.dotMultiply(this.inputs, this.weights).map(product => product + this.bias);
+			return math.dotMultiply(this.inputs, this.weights).map(n => n === -0 ? 0 : n);
 		}
 		get activatedInputs () {
-			// console.log("activatedInputs called");
-			if (!this.activationFn) {
-				return this.weightedInputs;
-			}
-			return this.activationFn(this.inputs); 
+			return this.activationFn(this.weightedInputs).map(n => n === -0 ? 0 : n); 
 		}
 		get outputSignal () {
-			// console.log("outputSignal called");
-			if (!this.activationFn) {
-				console.log("No activation function present, returning raw inputs...");
-				return this.inputs;
-			}
-			return math.sum(this.activatedInputs);
+			const weightedInputSum = math.sum(this.weightedInputs);
+			return this.activationFn(weightedInputSum + this.bias);
 		}
 		updateWeightsAndBias (actual) {
 			const costPrime = this.costFn(this.activatedInputs, this.actual, true);
-			console.log('costPrime', costPrime);
+			
 			const deltaB = this.activationFn(this.weightedInputs, true).reduce((accum, weight, i) => accum + weight * costPrime[i])
-			console.log('deltaB', deltaB);
-			// console.log('this.weightedInputs', this.weightedInputs);
+			
+			// 
 			const deltaW = this.inputs.map((input, i) => {
-				console.log('input', input)
+				
 				let result = this.activationFn(this.weightedInputs[i], true) * input * costPrime[i];
 				
-				console.log('result', result);
+				
 				return result;
 			});
-			console.log('deltaW', deltaW)
+			
 			const newWeights = this.weights.map((weight, i) => {
-				console.log('> deltaW[i]', deltaW[i]);
-				console.log('weight =>', weight);
 				weight -= this.learningRate * deltaW[i];
-				console.log('deltaW[i]', deltaW[i]);
-				console.log('new weight', weight);
 				return weight
 			});
-			console.log('newWeights', newWeights);
 			const newBias = this.bias -= this.learningRate * deltaB;
 			this.weights = newWeights;
 			this.bias = newBias;
-			console.log('costPrime b', costPrime);
-			console.log('actual', actual);
 			return actual;
 		}
 	}
@@ -137,12 +112,23 @@ const NeuralNetwork = (() => {
 	class InputNeuron extends Neuron {
 		constructor (initialValue) {
 			super(initialValue, null);
+			this.weights = null;
+			this.bias = null;
+		}
+		get weightedInputs () {
+			return this.inputs;
+		}
+		get activatedInputs () {
+			return this.inputs;
+		}
+		get outputSignal () {
+			return this.inputs;
 		}
 	}
 
 	class HiddenNeuron extends Neuron {
-		constructor (inputLayer) {
-			super(inputLayer, softmax);
+		constructor (inputLayer, activationFn = reLu) {
+			super(inputLayer, activationFn);
 		}
 	}
 
@@ -153,17 +139,17 @@ const NeuralNetwork = (() => {
 			this.classLabels = classLabels;
 		}
 		get error () {
-			console.log("$$");
-			console.log('this.actual', this.actual);
-			console.log('this.outputSignal', this.outputSignal);
+			
+			
+			
 			return this.costFn(this.outputSignal, this.actual);
 		}
 		get prediction () {
 			const certainty = math.max(this.outputSignal);
 			const labelIndex =  this.outputSignal.indexOf(certainty) - 1;
 			const prediction = this.classLabels[labelIndex];
-			console.log('prediction', prediction);
-			console.log('certainty', certainty);
+			
+			
 			return [prediction, certainty];
 		}
 	}
@@ -179,42 +165,48 @@ const NeuralNetwork = (() => {
 			return this.neurons.map(neuron =>  neuron.weights);
 		}
 		backprop (actuals) {
-			console.log('actual in backprop callL:', actuals)
+			
 			let output =  this.neurons.map((neuron, i) => neuron.updateWeightsAndBias(actuals[i] || actuals[actuals.length - 1]));
-			console.log('output', output);
+			
 			return output;
 
 		}
 	}
 
-	const layer0 = new Layer(InputNeuron, 9, 1);
-	console.log('\nlayer0', layer0.outputSignal);
+	// const layer0 = new Layer(InputNeuron, 9, 1);
+	// console.log('\nlayer0', layer0.outputSignal);
 
-	const layer1 = new Layer(HiddenNeuron, 9, layer0.outputSignal);
-	console.log('\nlayer1', layer1.outputSignal);
+	// const layer1 = new Layer(HiddenNeuron, 9, layer0.outputSignal);
+	// console.log('\nlayer1', layer1.outputSignal);
 
-	const layer2 = new Layer(HiddenNeuron, 8, layer1.outputSignal);
-	console.log('\nlayer2', layer2.outputSignal);
+	// const layer2 = new Layer(HiddenNeuron, 8, layer1.outputSignal);
+	// console.log('\nlayer2', layer2.outputSignal);
 
-	const layer3 = new Layer(HiddenNeuron, 3, layer2.outputSignal);
-	console.log('\nlayer3', layer3.outputSignal);
+	// const layer3 = new Layer(HiddenNeuron, 3, layer2.outputSignal);
+	// console.log('\nlayer3', layer3.outputSignal);
 
-	const layer4 = new Layer(OutputNeuron, 1, layer3.outputSignal);
-	console.log('\nlayer4', layer4.outputSignal);
+	// const layer4 = new Layer(OutputNeuron, 1, layer3.outputSignal);
+	// console.log('\nlayer4', layer4.outputSignal);
 
-	layer1.backprop(layer2.backprop(layer3.backprop(layer4.backprop([0.33]))));
+	// layer1.backprop(layer2.backprop(layer3.backprop(layer4.backprop([0.33]))));
 
 	// console.log('layer4.weights', layer4.weights);
 	// layer4.backprop([0.33]);
 	// console.log('layer4.weights', layer4.weights);
-	return { crossEntropyCostFunction,
+	return { 
+		adjustedRandomGaussian,
+		reLu,
+		softmax,
+		crossEntropyCostFunction,
 		Neuron,
-		layer4
+		InputNeuron,
+		HiddenNeuron,
+		OutputNeuron,
+		Layer
 	}
 	
 
 })();
 
-let lossTest = NeuralNetwork.crossEntropyCostFunction([0.55, 0.99, 0.001], 1);
-console.log('lossTest', lossTest);
+module.exports = NeuralNetwork;
 
