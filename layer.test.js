64919cf67a5f66ee15ce299c7400225f4a0a4458
Dennis Hodges <fermentationist@gmail.com>
2018-08-01 16:24:59 -0500

    Fixing matrix multiplication error in weightedInputs function discovered during Layer testing.

--- layer.test.js
+++ layer.test.js
@@ -9,9 +9,18 @@
 
 const layer0 = new Layer(InputNeuron, 9, [0, 0, 1, 0, 0, 0, 0, 0, 0]);
 const layer1 = new Layer(HiddenNeuron, 8, layer0.outputSignal);
+const layer2 = new Layer(HiddenNeuron, 3, layer1.outputSignal);
+const layer3 = new Layer(OutputNeuron, 1, layer2.outputSignal);
 test("Layer(InputNeuron, 9, [0, 0, 1, 0, 0, 0, 0, 0, 0]).neurons.length = 9", () => {
 	expect(layer0.neurons.length).toBe(9);
 });
 test("Layer(HiddenNeuron, 8, layer0.outputSignal).neurons.length = 8", () => {
 	expect(layer1.neurons.length).toBe(8);
 });
+test("Layer(HiddenNeuron, 3, layer1.outputSignal).neurons.length = 3", () => {
+	expect(layer2.neurons.length).toBe(8);
+});
+test("Layer(OutputNeuron, 1, layer2.outputSignal).neurons.length = 1", () => {
+	expect(layer2.neurons.length).toBe(8);
+});
+console.log(layer3.outputSignal);
