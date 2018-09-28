32618d807b2136e0cb40a4623d77f47b8db88a2d
Dennis Hodges <fermentationist@gmail.com>
2018-09-28 13:07:36 -0500

    helper library 'handled.js' finished. Will create separate Github repo for it.

--- handledtest.js
+++ handledtest.js
@@ -1,35 +1,64 @@
-const {handled, assignPropShortcut} = require("./handled.js");
-assignPropShortcut("Ø");
+const handled = require("./handled.js");
 
-// Object.defineProperty( Object.prototype, "E", {
-// 	get: function (){
-// 		return handled(this);
-// 	}
-// });
-
-const testF = async (input) => {
-	// const delayedOutput = await handled2((setTimeout(()=> Promise.reject(new Error("fuck!!")), 1000)))
+const rejectedPromise = (async (input) => {
 	const delayedOutput = await new Promise((resolve, reject) => {
-		setTimeout(() => reject(input), 500)
-		// reject("d'oh!"))
+		setTimeout(() => reject(input), 750)
 	})
 	return delayedOutput;
-}
+}).ø;
 
-let testS = async (input) => {
-	// const delayedOutput = await handled2((setTimeout(()=> Promise.reject(new Error("fuck!!")), 1000)))
+let resolvingPromise = (async (input) => {
 	const delayedOutput = await new Promise((resolve, reject) => {
-		setTimeout(() => resolve(input), 500)
-		// reject("d'oh!"))
+		setTimeout(() => resolve(input), 750)
 	})
 	return delayedOutput;
-}
+});
 
+let plainFunction = () => console.log("hello from plainFunction");
+let errorFunction = () => {throw Error("errorFunction error!")};
+errorFunction = errorFunction.ø
+plainFunction = plainFunction.ø;
+// console.log('plainFunction', plainFunction);
+// console.log('errorFunction', errorFunction);
+let nonF = {answer: 42}
+let arrayOfPromises = [
+	rejectedPromise("D'oh!"),
+	resolvingPromise("D'uh!")
+]
+let arrayOfInts = [ 0, 1, 2 ];
+// const testPromise = {}//new Promise(()=>{});
+// console.log(`typeof testPromise = ${testPromise.then(()=>{})}`);
+rejectedPromise("Ho!");
+rejectedPromise("Hey!").ø;
+resolvingPromise("howdy!").ø;
+nonF === nonF.ø;
+arrayOfInts === arrayOfInts.ø;
+arrayOfPromises === arrayOfPromises.ø;
+// console.log('arrayOfPromises', arrayOfPromises);
+// console.log('arrayOfInts', arrayOfInts);
+// console.log('nonF', nonF);
 
-const nonF = {answer: 42}
+let promiseArray2 = [
+	rejectedPromise("rejected 1"),
+	resolvingPromise("resolving 2"),
+	rejectedPromise("rejected 3"),
+	resolvingPromise("resolving 4"),
+	rejectedPromise("rejected 5"),
+	resolvingPromise("resolving 6")
+].ø;
 
+// console.log(promiseArray2)
 
-testF("hello?").Ø.then(testF("what?").Ø).then(testS("how?").Ø)
+// setTimeout(()=>console.log(promiseArray2), 2000)
+// rejectedPromise("hello?").ø.then(rejectedPromise("what?").ø).then(resolvingPromise("how?").ø);
+
+// const promises = [
+// 	rejectedPromise("hello?"),
+// 	rejectedPromise("what?"),
+// 	resolvingPromise("how?")
+// ]
+
+// handleAll(promises);
 
 
 
